e83689e22e8cd202269317b8700ec4786db39953
AishwaryaAutkar <106028537+aish5612@users.noreply.github.com>
2023-05-29 01:19:18 +0530

    Update 01 XOR.py

--- 01 XOR.py
+++ 01 XOR.py
@@ -12,3 +12,26 @@ def operation(s):
     print(ans_and)
 
 operation("hello world")
+
+
+
+
+
+
+void main(){
+    char str[]="Hello World";
+    int i,len;
+    len = strlen(str);
+    for(i=0;i<len;i++){
+            printf("%c",str[i]&127);
+    }
+    printf("\n");
+    for(int i=0;i<len;i++){
+            printf("%c",str[i]^127);
+    }
+    printf("\n");
+    for(int i=0;i<len;i++){
+            printf("%c",str[i]|127);
+    }
+    printf("\n");
+}
